0a8cdfe061b0191547c5dab62cf48f405274618d
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2022-06-30 16:05:05 -0500

    disocvery page updated

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
-
 import './css-files/discover.css';
 
-// This holds a list of some fiction people
-// Some  have the same name but different age and id
 const USERS = [
-  { id: 1, name: 'Pizza Hut', distance: 32 },
-  { id: 2, name: 'Dominos', distance: 30 },
-  { id: 3, name: 'Chipotle', distance: 40 },
-  { id: 4, name: 'Taco Bell', distance: 50 },
-  { id: 5, name: 'McDonalds', distance: 30 },
-  { id: 6, name: 'Chick-Fil-A', distance: 68 },
-  { id: 7, name: 'Popeyes', distance: 34 },
-  { id: 8, name: 'Panda Express', distance: 28 },
-  { id: 9, name: 'Starbucks', distance: 23 },
-];
+  {id:1 , name: "Pizza hut", distance:2 , food: "Pizza"},
+  {id:2 , name: "Dominos", distance:3 , food: "Pizza"},
+  {id:3 , name: "Chiptole", distance:4 , food: "Mexican"},
+  {id:4 , name: "Taco Bell", distance:5 , food: "Mexican"},
+  {id:5 , name: "Mcdonalds", distance:6 , food: "Burgers"},
+  {id:6 , name: "Starbucks", distance:7 , food: "Coffee"},
+  {id:7 , name: "Chick-Fil-A", distance:8 , food: "Chicken"},
+  {id:8 , name: "Popeyes", distance:9 , food: "Chicken"},
+  {id:9 , name: "Panda Express", distance:10 , food: "Chinese"},
+  {id:10 , name: "Sushi", distance:11 , food: "Fish"}
+]
 
 function Discover() {
   // the value of the search field 
@@ -39,9 +37,10 @@ function Discover() {
 
     setName(keyword);
   };
-
+  
   return (
     <div className="container">
+      
       <input
         type="search"
         value={name}
@@ -54,7 +53,7 @@ function Discover() {
         {foundUsers && foundUsers.length > 0 ? (
           foundUsers.map((user) => (
             <li key={user.id} className="user">
-              <span className="user-id">{user.id}</span>
+              <span style={{width:100}} className="user-id">{user.id}</span><button style={{width:30}}></button>
               <span className="user-name">{user.name}</span>
               <span className="user-age">{user.distance} miles away</span>
             </li>
@@ -63,8 +62,11 @@ function Discover() {
           <h1>No results found!</h1>
         )}
       </div>
+      <filter />
     </div>
+    
   );
 }
 
 export default Discover;
+

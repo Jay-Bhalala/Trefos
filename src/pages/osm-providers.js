211ca26ac6b80986a556963eeee4c42ba471e181
Jay1532 <bhalalajay722@gmail.com>
2022-06-30 17:19:57 -0500

    update map

--- src/pages/osm-providers.js
+++ src/pages/osm-providers.js
@@ -1,6 +1,6 @@
 export default{
     maptiler: {
         url: "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=C7rT5BMlpgDEIUGlmSKv",
-        attribution: 'https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=C7rT5BMlpgDEIUGlmSKv'
+        attribution: 'https://api.maptiler.com/maps/basic/?key=C7rT5BMlpgDEIUGlmSKv#-0.2/0.00000/61.69922'
     }
 }

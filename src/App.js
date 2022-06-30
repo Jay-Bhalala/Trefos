8db756e111a6f64ecc655c550128eae43cb4a795
JeremyChuah <JeremyChuah15@gmail.com>
2022-06-29 22:16:53 -0500

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import NavbarNoLogIn from "./navbars/NavbarNoLogIn.js";
 import NavbarFoodBank from "./navbars/NavbarFoodBank.js";
 import NavbarRestaurant from "./navbars/NavbarRestaurant.js";
-import Home from "./pages/Home/Home";
 
 import Home from "./pages/Home.js";
 import LoginSignupNonProfit from "./pages/LoginSignupNonProfit.js";
@@ -19,36 +18,121 @@ import Map from "./pages/Map.js";
 function App() {
   return (
     <>
-<<<<<<< Updated upstream
       <Routes>
-
         <Route exact path="/nologin" element={<NavbarNoLogIn />} />
-        <Route exact path="/nologin/home" element={<><NavbarNoLogIn /><Home/></>} />
-        <Route exact path="/nologin/loginsignupfoodbank" element={<><NavbarNoLogIn /><LoginSignupNonProfit /></>} />
-        <Route exact path="/nologin/loginsignuprestaurant" element={<><NavbarNoLogIn /><LoginSignupRestaurant /></>} />
-        <Route exact path="/nologin/aboutus" element={<><NavbarNoLogIn /><AboutUs /></>} />
-        <Route exact path="/nologin/contactus" element={<><NavbarNoLogIn /><Contact /></>} />
+        <Route
+          exact
+          path="/nologin/home"
+          element={
+            <>
+              <NavbarNoLogIn />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/nologin/loginsignupfoodbank"
+          element={
+            <>
+              <NavbarNoLogIn />
+              <LoginSignupNonProfit />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/nologin/loginsignuprestaurant"
+          element={
+            <>
+              <NavbarNoLogIn />
+              <LoginSignupRestaurant />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/nologin/aboutus"
+          element={
+            <>
+              <NavbarNoLogIn />
+              <AboutUs />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/nologin/contactus"
+          element={
+            <>
+              <NavbarNoLogIn />
+              <Contact />
+            </>
+          }
+        />
 
         <Route path="/foodbank" element={<NavbarFoodBank />} />
-        <Route exact path="/foodbank/home" element={<><NavbarFoodBank /><Home/></>} />
-        <Route exact path="/foodbank/discover" element={<><NavbarFoodBank /><Discover /></>} />
-        <Route exact path="/foodbank/liked" element={<><NavbarFoodBank /><Liked /></>} />
-        <Route exact path="/foodbank/map" element={<><NavbarFoodBank /><Map /></>} />
-        <Route exact path="/foodbank/aboutus" element={<><NavbarFoodBank /><AboutUs /></>} />
-        <Route exact path="/foodbank/contactus" element={<><NavbarFoodBank /><Contact /></>} />
+        <Route
+          exact
+          path="/foodbank/home"
+          element={
+            <>
+              <NavbarFoodBank />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/foodbank/discover"
+          element={
+            <>
+              <NavbarFoodBank />
+              <Discover />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/foodbank/liked"
+          element={
+            <>
+              <NavbarFoodBank />
+              <Liked />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/foodbank/map"
+          element={
+            <>
+              <NavbarFoodBank />
+              <Map />
+            </>
+          }
+        />
 
-        <Route path="/restaurant" element={<><NavbarRestaurant /><RestaurantDashboard/></>} />
-        <Route exact path="/restaurant/home" element={<><NavbarRestaurant /><Home/></>} />
-
+        <Route
+          path="/restaurant"
+          element={
+            <>
+              <NavbarRestaurant />
+              <RestaurantDashboard />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/restaurant/home"
+          element={
+            <>
+              <NavbarRestaurant />
+              <Home />
+            </>
+          }
+        />
       </Routes>
-=======
-      <Home />
-      {/* <Routes>
-        <Route path="/nologin" element={<NavbarNoLogin />} />
-        <Route path="/nonprofit" element={<NavbarFoodBank />} />
-        <Route path="/restaurant" element={<NavbarRestaurant />} />
-      </Routes> */}
->>>>>>> Stashed changes
     </>
   );
 }

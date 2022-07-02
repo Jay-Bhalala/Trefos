125e8764bdbc0f86d7c034a7b32517339e106fab
JeremyChuah <JeremyChuah15@gmail.com>
2022-07-01 22:37:56 -0500

    Map update

--- src/pages/Restaraunt-Info/StaticMap.js
+++ src/pages/Restaraunt-Info/StaticMap.js
@@ -1,28 +1,27 @@
 import React from "react";
 
 import L from "leaflet";
-import { MapContainer as Map , Marker, TileLayer } from "react-leaflet";
+import { MapContainer as Map, Marker, TileLayer } from "react-leaflet";
 import osm from "../osm-providers";
 import "leaflet/dist/leaflet.css";
-import "./StaticMap.css"
+import "./StaticMap.css";
 
 const StaticMap = (props) => {
-  
-    delete L.Icon.Default.prototype._getIconUrl;
+  delete L.Icon.Default.prototype._getIconUrl;
 
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-      iconUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-      shadowUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
-    });
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    iconUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+  });
 
   let lat = props.lat1;
   let lng = props.lng1;
 
-  const marker = {lat, lng};
+  const marker = { lat, lng };
 
   return (
     <>
@@ -34,7 +33,7 @@ const StaticMap = (props) => {
             <Map
               center={marker}
               zoom={12}
-              className="static-map"
+              style={{ width: "100%", height: "16rem" }}
             >
               <TileLayer url={osm.maptiler.url} />
               <Marker position={[lat, lng]}></Marker>
--- src/pages/Restaraunt-Info/restaurantInfo.js
+++ src/pages/Restaraunt-Info/restaurantInfo.js
@@ -1,36 +1,40 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbTack, faPhone, faMailForward } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faThumbTack,
+  faPhone,
+  faMailForward,
+} from "@fortawesome/free-solid-svg-icons";
 import StaticMap from "./StaticMap.js";
-
 
 function RestaurantInfo(props) {
   return (
     <>
-    <script src="https://kit.fontawesome.com/59be458d1a.js" crossorigin="anonymous"></script>
-    <div >
-        <Card style={{ width: '30rem' }}>
-            {/* <Card.Img variant="top" src={Test} /> */}
-            <StaticMap 
-                lat1 = {props.lat2}
-                lng1 = {props.lng2}
-            />
-            <Card.Body>
-                <Card.Title>Contact Info</Card.Title>
-                <Card.Text>
-                    <FontAwesomeIcon icon={faThumbTack} />  Location: {props.address} 
-                </Card.Text>
-                <Card.Text>
-                    <FontAwesomeIcon icon={faPhone} />  Phone Number: {props.phoneNumber} 
-                </Card.Text>
-                <Card.Text>
-                    <FontAwesomeIcon icon={faMailForward} />  Email: {props.email} 
-                </Card.Text>
-                {/* <Button variant="primary"><FontAwesomeIcon icon={faMarker} /> Edit Info</Button> */}
-            </Card.Body>
+      <script
+        src="https://kit.fontawesome.com/59be458d1a.js"
+        crossorigin="anonymous"
+      ></script>
+      <div>
+        <Card style={{ width: "30rem", height: "auto" }}>
+          {/* <Card.Img variant="top" src={Test} /> */}
+          <StaticMap lat1={props.lat2} lng1={props.lng2} />
+          <Card.Body>
+            <Card.Title>Contact Info</Card.Title>
+            <Card.Text>
+              <FontAwesomeIcon icon={faThumbTack} /> Location: {props.address}
+            </Card.Text>
+            <Card.Text>
+              <FontAwesomeIcon icon={faPhone} /> Phone Number:{" "}
+              {props.phoneNumber}
+            </Card.Text>
+            <Card.Text>
+              <FontAwesomeIcon icon={faMailForward} /> Email: {props.email}
+            </Card.Text>
+            {/* <Button variant="primary"><FontAwesomeIcon icon={faMarker} /> Edit Info</Button> */}
+          </Card.Body>
         </Card>
-    </div>
+      </div>
     </>
   );
 }

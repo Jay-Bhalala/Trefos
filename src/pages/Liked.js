1ae27101adfb9ef3d8657ea289ed0d72df9bf386
Jay1532 <bhalalajay722@gmail.com>
2022-07-02 01:30:11 -0500

    yuhhhh

--- src/pages/Liked.js
+++ src/pages/Liked.js
@@ -1,103 +1,40 @@
 import React from "react";
 import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import RestaurantCard from "./RestaurantCard.js";
 
 function Liked(props) {
   return (
+    <>
+    <h2>Restaurants You Have Liked</h2>
     <div className="liked container">
       <MDBRow className='row-cols-1 row-cols-md-4 g-4'>
-        <MDBCol>
-          <MDBCard className='h-100'>
-            <MDBCardImage
-              src='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
-              alt='...'
-              position='top'
-            />
-            <MDBCardBody>
-              <MDBCardTitle><a href = "">Restaurant 1</a></MDBCardTitle>
-              <MDBCardText>
-                <img src="https://lh6.googleusercontent.com/NUsqW8Sd2gAeBI6Gj3lI2eF_QDz-r9khgd1vveKenSv6JmCWxoNEmmQrJrz-MyIYMU7pipA3trFLuiDvZLo8RWuRtXkLapTUWYkj_U892u6IlAhAowuLJJwc-kEcPsDhwUk6gg" width="15" height="auto"></img>
-                Medium <br></br>
-                <img src="https://image.pngaaa.com/328/1509328-small.png" width="15" height="auto"></img>
-                Frankston
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol>
-          <MDBCard className='h-100'>
-            <MDBCardImage
-              src='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
-              alt='...'
-              position='top'
-            />
-            <MDBCardBody>
-              <MDBCardTitle><a href = "">Restaurant 2</a></MDBCardTitle>
-              <MDBCardText>
-                <img src="https://lh6.googleusercontent.com/NUsqW8Sd2gAeBI6Gj3lI2eF_QDz-r9khgd1vveKenSv6JmCWxoNEmmQrJrz-MyIYMU7pipA3trFLuiDvZLo8RWuRtXkLapTUWYkj_U892u6IlAhAowuLJJwc-kEcPsDhwUk6gg" width="15" height="auto"></img>
-                Medium <br></br>
-                <img src="https://image.pngaaa.com/328/1509328-small.png" width="15" height="auto"></img>
-                Frankston
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol>
-          <MDBCard className='h-100'>
-            <MDBCardImage
-              src='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
-              alt='...'
-              position='top'
-            />
-            <MDBCardBody>
-              <MDBCardTitle><a href = "">Restaurant 3</a></MDBCardTitle>
-              <MDBCardText>
-                <img src="https://lh6.googleusercontent.com/NUsqW8Sd2gAeBI6Gj3lI2eF_QDz-r9khgd1vveKenSv6JmCWxoNEmmQrJrz-MyIYMU7pipA3trFLuiDvZLo8RWuRtXkLapTUWYkj_U892u6IlAhAowuLJJwc-kEcPsDhwUk6gg" width="15" height="auto"></img>
-                Medium <br></br>
-                <img src="https://image.pngaaa.com/328/1509328-small.png" width="15" height="auto"></img>
-                Frankston
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol>
-          <MDBCard className='h-100'>
-            <MDBCardImage
-              src='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
-              alt='...'
-              position='top'
-            />
-            <MDBCardBody>
-              <MDBCardTitle><a href = "">Restaurant 4</a></MDBCardTitle>
-              <MDBCardText>
-                <img src="https://lh6.googleusercontent.com/NUsqW8Sd2gAeBI6Gj3lI2eF_QDz-r9khgd1vveKenSv6JmCWxoNEmmQrJrz-MyIYMU7pipA3trFLuiDvZLo8RWuRtXkLapTUWYkj_U892u6IlAhAowuLJJwc-kEcPsDhwUk6gg" width="15" height="auto"></img>
-                Medium <br></br>
-                <img src="https://image.pngaaa.com/328/1509328-small.png" width="15" height="auto"></img>
-                Frankston
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol>
-          <MDBCard className='h-100'>
-            <MDBCardImage
-              src='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
-              alt='...'
-              position='top'
-            />
-            <MDBCardBody>
-              <MDBCardTitle><a href = "">Restaurant 1</a></MDBCardTitle>
-              <MDBCardText>
-                <img src="https://lh6.googleusercontent.com/NUsqW8Sd2gAeBI6Gj3lI2eF_QDz-r9khgd1vveKenSv6JmCWxoNEmmQrJrz-MyIYMU7pipA3trFLuiDvZLo8RWuRtXkLapTUWYkj_U892u6IlAhAowuLJJwc-kEcPsDhwUk6gg" width="15" height="auto"></img>
-                Medium <br></br>
-                <img src="https://image.pngaaa.com/328/1509328-small.png" width="15" height="auto"></img>
-                Frankston
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
+        <RestaurantCard
+          restaurantName="Sample Restaurant"
+          src1='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
+          href1="/foodbank/liked/sample-restaurant-info"
+          state1="false"
+        />
+        <RestaurantCard
+        restaurantName="Sample Restaurant"
+        src1='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
+        href1="/foodbank/liked/sample-restaurant-info"
+        state1="false"
+        />
+        <RestaurantCard
+        restaurantName="Sample Restaurant"
+        src1='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
+        href1="/foodbank/liked/sample-restaurant-info"
+        state1="false"
+        />
+        <RestaurantCard
+        restaurantName="Sample Restaurant"
+        src1='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
+        href1="/foodbank/liked/sample-restaurant-info"
+        state1="false"
+        />
       </MDBRow>
     </div>
-
+    </>
   );
 }
 
---
+++ src/pages/RestaurantCard.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCol } from 'mdb-react-ui-kit';
+import Badge from 'react-bootstrap/Badge';
+import { MDBBtn } from 'mdb-react-ui-kit';
+import ToggleButton from 'react-toggle-button'
+
+
+function RestaurantCard(props) {
+  const [state, setState] = React.useState(props.state1)
+
+  return (   
+    <>    
+       <MDBCol>
+          <MDBCard className='h-100'>
+            <MDBCardImage
+              src={props.src1}
+              alt='...'
+              position='top'
+            />
+            <MDBCardBody className="text-center">
+
+
+
+
+
+
+
+
+              <MDBCardTitle>
+                <a href={props.href1}>{props.restaurantName}   </a></MDBCardTitle>
+                {/* <div onClick={setState(!state)}>
+                    {state ? <Badge bg="success" className="justify-content-end">Liked</Badge> : <Badge bg="secondary" className="justify-content-end">Not Liked</Badge>}
+                </div> */}
+                <Badge bg="success" className="justify-content-end">Liked</Badge>
+
+
+
+
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+    </>
+    );
+}
+
+export default RestaurantCard;

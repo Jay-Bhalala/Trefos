6da7c79771e8ca6e2aba2b7bd5541ff35225363a
JeremyChuah <JeremyChuah15@gmail.com>
2022-07-07 14:25:16 -0500

    small styling chnages

--- src/pages/Liked/LikeButton.js
+++ src/pages/Liked/LikeButton.js
@@ -4,33 +4,29 @@ import { faHeartBroken } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class Like extends Component {
-state = { liked: false };
-toggle = () => {
-  let localLiked = this.state.liked;
+  state = { liked: false };
+  toggle = () => {
+    let localLiked = this.state.liked;
 
-  // Toggle the state variable liked
-  localLiked = !localLiked;
-  this.setState({ liked: localLiked });
-};
-render() {
-  return (
-  <div className="container">
-    <center>
-    <div
-      className="container"
-      style={{ border: "1px solid black", width: "15%" }}
-      onClick={() => this.toggle()}
-    >
-      {this.state.liked === false ? (
-      <FontAwesomeIcon icon={faHeart} />
-      ) : (
-      <FontAwesomeIcon icon={faHeartBroken} />
-      )}
-    </div>
-    </center>
-  </div>
-  );
-}
+    // Toggle the state variable liked
+    localLiked = !localLiked;
+    this.setState({ liked: localLiked });
+  };
+  render() {
+    return (
+      <div>
+        <center>
+          <div style={{ width: "15%" }} onClick={() => this.toggle()}>
+            {this.state.liked === false ? (
+              <FontAwesomeIcon icon={faHeart} />
+            ) : (
+              <FontAwesomeIcon icon={faHeartBroken} />
+            )}
+          </div>
+        </center>
+      </div>
+    );
+  }
 }
 
 export default Like;
--- src/pages/Liked/Liked.js
+++ src/pages/Liked/Liked.js
@@ -1,20 +1,30 @@
 import React from "react";
-import { MDBRow } from 'mdb-react-ui-kit';
+import { MDBRow } from "mdb-react-ui-kit";
 import RestaurantCard from "../RestaurantCard.js";
 
 function Liked(props) {
   return (
     <>
-    <h2>Restaurants You Have Liked</h2>
-    <div className="liked container">
-      <MDBRow className='row-cols-1 row-cols-md-4 g-4'>
-        <RestaurantCard
-          restaurantName="Sample Restaurant"
-          src1='https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg'
-          href1="/foodbank/liked/sample-restaurant-info"
-        />
-      </MDBRow>
-    </div>
+      <h2>Restaurants You Have Liked</h2>
+      <div>
+        <MDBRow className="row-cols-1 row-cols-md-4 g-4">
+          <RestaurantCard
+            restaurantName="Sample Restaurant"
+            src1="https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg"
+            href1="/foodbank/liked/sample-restaurant-info"
+          />
+          <RestaurantCard
+            restaurantName="Sample Restaurant"
+            src1="https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg"
+            href1="/foodbank/liked/sample-restaurant-info"
+          />
+          <RestaurantCard
+            restaurantName="Sample Restaurant"
+            src1="https://alderuccio.com.au/wp-content/uploads/2017/06/crust-gourmet-pizza-logo-jpg.jpg"
+            href1="/foodbank/liked/sample-restaurant-info"
+          />
+        </MDBRow>
+      </div>
     </>
   );
 }

7ae08b7a6dfdbd806fa0d1d3fe1a095ff2dd882a
JeremyChuah <JeremyChuah15@gmail.com>
2022-07-18 23:29:41 -0500

    stop whining jay

--- src/pages/Home/impactCard.js
+++ src/pages/Home/impactCard.js
@@ -6,34 +6,36 @@ import connectIcon from "./connectIcon.svg";
 function impactCard(props) {
   return (
     <div className="large-container">
-      <div>
-        <div className="impact-text">
-          <h1>Connecting The Community</h1>
+      <div className="connecting-text-container">
+        <div>
+          <p style={{ fontSize: "4rem", fontWeight: "bold" }}>
+            Connecting The Community
+          </p>
         </div>
-        <div className="impact-text-card">
-          <div>
-            <img src={connectIcon} className="image" />
-          </div>
-          <div className="little-text">
-            <p className="heading-text-container">Restaurants</p>
+        <div className="impact-text-container">
+          <div className="restaurant-text-container">
+            <h1>Restaurants</h1>
             <p>
-              Restaurants can create a profile and update their food stocks when they have food avaliable to donate for all food banks on the platform to view.
+              Restaurants can create a profile and update their food stocks when
+              they have food avaliable to donate for all food banks on the
+              platform to view.
             </p>
           </div>
         </div>
-        <div className="impact-text-card">
-          <div>
-            <img src={connectIcon} className="image" />
-          </div>
-          <div className="little-text">
-            <p className="heading-text-container">Food Banks</p>
+        <div>
+          <img src={connectIcon} />
+        </div>
+        <div className="impact-text-container">
+          <div className="restaurant-text-container">
+            <h1>Food Banks</h1>
             <p>
-              Food banks don't need to sign up and can discover restaurants on the platform with just the touch of a button!
+              Food banks don't need to sign up and can discover restaurants on
+              the platform with just the touch of a button!
             </p>
           </div>
         </div>
       </div>
-      <div>
+      <div className="help-image-container">
         <img src={foodBank} className="image-hidden" />
       </div>
     </div>

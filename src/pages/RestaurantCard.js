bd07bb9a4b38b25d6658463b2aef95ebec53cf51
Jay Bhalala <bhalalajay722@gmail.com>
2022-08-01 18:02:12 -0500

    capitalization

--- src/pages/RestaurantCard.js
+++ src/pages/RestaurantCard.js
@@ -63,7 +63,7 @@ function RestaurantCard(props) {
                   fontWeight: "lighter",
                 }}
               >
-                {props.pounds} Pounds of food avaiable
+                {props.pounds} Pounds Of Food Avaiable
               </p>
             </MDBCardTitle>
           </MDBCardBody>

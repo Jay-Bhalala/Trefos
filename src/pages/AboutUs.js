6cdeec7aff1aa602d4f5521faf3feb061a17a75b
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2022-09-17 16:05:31 -0500

    pictures fix

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -44,7 +44,7 @@ function AboutUs(props) {
             <div class="team_img">
               <img
                 class="rounded-circle z-depth-2"
-                src="https://media-exp1.licdn.com/dms/image/C4E03AQEoAq_eO6uAiA/profile-displayphoto-shrink_400_400/0/1619297408553?e=1662595200&v=beta&t=6PeG9fwDDA5OGlePVlTyRWxQQLFEg8ZuJ-ot6__ysDA"
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQEoAq_eO6uAiA/profile-displayphoto-shrink_200_200/0/1619297408553?e=1668643200&v=beta&t=RMhJsU5ucbc85vhseDhshMVxzfaOse9vAv0JdL3q-gc"
                 alt="Team_image"
               />
             </div>
@@ -55,7 +55,7 @@ function AboutUs(props) {
             <div class="team_img">
               <img
                 class="rounded-circle z-depth-2"
-                src="https://media-exp1.licdn.com/dms/image/C5603AQGSH3NUBJpvAw/profile-displayphoto-shrink_400_400/0/1650497222877?e=1662595200&v=beta&t=8bCzHiEOVLlbSUl1FQrLxhRgV3Ue3vyxzRpWJbslqW8"
+                src="https://media-exp1.licdn.com/dms/image/C5603AQGSH3NUBJpvAw/profile-displayphoto-shrink_200_200/0/1650497222877?e=1668643200&v=beta&t=d_jbqg9q0elRktFKYh-lbiTMWFL78FtSIddsYAaF4Es"
                 alt="Team_image"
               />
             </div>
@@ -66,7 +66,7 @@ function AboutUs(props) {
             <div class="team_img">
               <img
                 class="rounded-circle z-depth-2"
-                src="https://media-exp1.licdn.com/dms/image/C4D03AQGNVANBE7dCww/profile-displayphoto-shrink_400_400/0/1655150440686?e=1662595200&v=beta&t=gQU7-bD29WXv2fL56lruwOOfGdgtU1-bs-uNITl75rg"
+                src="https://media-exp1.licdn.com/dms/image/C4D03AQGNVANBE7dCww/profile-displayphoto-shrink_400_400/0/1655150440686?e=1668643200&v=beta&t=pGWoG_VGd0dlqU7Eu65mWkuNaHVesr4MXSaHM-olAU0"
                 alt="Team_image"
               />
             </div>
@@ -77,12 +77,12 @@ function AboutUs(props) {
             <div class="team_img">
               <img
                 class="rounded-circle z-depth-2"
-                src="https://media-exp1.licdn.com/dms/image/C4E03AQHDveFR7dRwbw/profile-displayphoto-shrink_400_400/0/1646675440410?e=1662595200&v=beta&t=rZAoNVorbdytFF2xMpXyUiqEHABn3hNQ4o7twpHAejU"
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQHDveFR7dRwbw/profile-displayphoto-shrink_200_200/0/1646675440410?e=1668643200&v=beta&t=5MrvQ1u19uIm4O9_plfXId11V7rK57SlWRYVkLrQQjU"
                 alt="Team_image"
               />
             </div>
             <h3>Nikhil Jain</h3>
-            <p class="role">Growth</p>
+            <p class="role">Marketing Lead</p>
           </div>
         </div>
       </div>
